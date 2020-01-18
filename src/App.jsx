2ff1be27cee26d7944db288ed58d8236b86b614a
markbello <mcbello51286@gmail.com>
2020-01-18 15:59:22 -0500

    add Header and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const App = () => <h1>Hello World</h1>;
+const App = () => {
+  const GREETING = 'Hello, world!';
+  
+  return (
+    <div>
+      <Header />
+      <h1>{GREETING}</h1>
+      <Footer />
+    </div>
+  );
+};
 
 export default hot(module)(App);
