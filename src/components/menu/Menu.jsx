8d711387eb06244db19e7ac0d270ebec0bfe4c10
Mac <mac@Macs-MacBook-Pro-2.local>
2020-02-08 13:44:53 -0500

    feat(Menu); add Menu component

---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+
+const Menu = () => (
+  <div className="menucontainer">
+    <h1>Menu</h1>
+    <ul>
+      <MenuItem />   
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+      <MenuItem />
+    </ul> 
+  </div>
+  
+  
+);
+export default Menu;
---
+++ src/components/menu/MenuItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MenuItem = () => (
+  <li>        
+    <p className="name">name</p>                          
+    <p className="description">description</p>
+    <p className="price">price</p>
+  </li>
+  
+);
+
+export default MenuItem;
