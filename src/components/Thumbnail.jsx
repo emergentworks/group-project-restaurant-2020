7ea48225e18638860195827de1fca05c31b7c701
skylar <skystalls1122@gmail.com>
2020-02-08 17:12:06 -0500

    file changes

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Thumbnail = () => (
-  <div class="media">
-   <div class="media-left">
+  <div className="media">
+   <div className="media-left">
      <a href="#">
-       <img class="media-object" src="https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="..." />
+       <img className="media-object" src="https://images.unsplash.com/photo-1534422298391-e4f8c172dddb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="..." />
      </a>
    </div>
-   <div class="media-body">
-     <h4 class="media-heading">Gastronomy atmosphere set aside. Slice butternut cooking home. Delicious romantic undisturbed raw platter will meld. Thick Skewers skillet natural, smoker soy sauce wait roux. slices rosette bone-in simmer precision alongside baby leeks. Crafting renders aromatic enjoyment, then slices taco. Minutes undisturbed cuisine lunch magnificent mustard curry. Juicy share baking sheet pork. Meals ramen rarities selection, raw pastries richness magnificent atmosphere. Sweet soften dinners, cover mustard infused skillet, Skewers on culinary experienc</h4>
+   <div className="media-body">
+     <h4 className="media-heading">Gastronomy atmosphere set aside. Slice butternut cooking home. Delicious romantic undisturbed raw platter will meld. Thick Skewers skillet natural, smoker soy sauce wait roux. slices rosette bone-in simmer precision alongside baby leeks. Crafting renders aromatic enjoyment, then slices taco. Minutes undisturbed cuisine lunch magnificent mustard curry. Juicy share baking sheet pork. Meals ramen rarities selection, raw pastries richness magnificent atmosphere. Sweet soften dinners, cover mustard infused skillet, Skewers on culinary experienc</h4>
      ...
    </div>
   </div>
